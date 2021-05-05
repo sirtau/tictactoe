17e3f27defa3302a2b17d197e3b9d8eb6c837375
sirtau <contact@hamidmousa.com>
2021-05-05 19:45:27 +1000

    Version 0.2
    added win counter
    fixed player alternating when clicking on square that has a move inside it
    added draw state

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-let  currentPlayer = "X";
+let currentPlayer = "X";
 let gameInProgress = true
 console.log(currentPlayer)
 let moveCounter = 0
+let xWins = 0
+let oWins = 0
+
+const winCounter = document.querySelector('#winCounter')
+winCounter.innerText = "X Wins: " + xWins + "\nO Wins: " + oWins
+
+const updateMessage = document.querySelector('#updateMessage')
+updateMessage.innerText = "Welcome! X goes first."
+
 
 
 const resetButton = document.querySelector('#resetButton')
@@ -19,10 +28,6 @@ const cell6 = document.querySelector('#c6')
 const cell7 = document.querySelector('#c7')
 const cell8 = document.querySelector('#c8')
 
-
-
-
-
 const cells = document.querySelectorAll('.cells')
 
 for (i = 0; i < cells.length; i++) {
@@ -30,19 +35,24 @@ for (i = 0; i < cells.length; i++) {
 }
 
 function turnExecute(event) {
-     
-    playerInput(event.target)
-    winCheck()
-    alternatePlayer()
+    if (gameInProgress == true && event.target.innerText == '') {
+        playerInput(event.target)
+        winCheck()
+        //        console.log(event.target.innerText)
+
+
+    }
 }
 
 
 function alternatePlayer() {
     if (currentPlayer == 'X') {
         currentPlayer = 'O'
+        updateMessage.innerText = "It's O's Turn."
         //        console.log(currentPlayer)
     } else {
         currentPlayer = 'X'
+        updateMessage.innerText = "It's X's Turn."
         //        console.log(currentPlayer)
     }
 }
@@ -52,39 +62,50 @@ function winCheck() {
     if (gameInProgress == false) {
 
     } else if ('' != cell0.innerText && cell0.innerText == cell1.innerText && cell1.innerText == cell2.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell3.innerText && cell3.innerText == cell4.innerText && cell4.innerText == cell5.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell6.innerText && cell6.innerText == cell7.innerText && cell7.innerText == cell8.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell0.innerText && cell0.innerText == cell3.innerText && cell3.innerText == cell6.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell1.innerText && cell1.innerText == cell4.innerText && cell4.innerText == cell7.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell2.innerText && cell2.innerText == cell5.innerText && cell5.innerText == cell8.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell0.innerText && cell0.innerText == cell4.innerText && cell4.innerText == cell8.innerText) {
-        console.log(currentPlayer + " Wins!")
-        gameInProgress = false
+        winCountUp()
     } else if ('' != cell2.innerText && cell2.innerText == cell4.innerText && cell4.innerText == cell6.innerText) {
-        console.log(currentPlayer + " Wins!")
+        winCountUp()
+    } else if (moveCounter == 9) {
+        updateMessage.innerText = "DRAW! Click reset to try again."
         gameInProgress = false
-    }
+    } else alternatePlayer()
 }
 
 function playerInput(cells) {
     if (gameInProgress == true && cells.innerText == '') {
         cells.innerText = currentPlayer
+        moveCounter++
+        console.log(moveCounter)
     }
     //    console.log(currentPlayer)
 }
 
+
+function winCountUp() {
+
+    console.log(currentPlayer + " Wins!")
+    updateMessage.innerText = currentPlayer + " Wins!"
+
+    if (currentPlayer == 'X') {
+        xWins++
+    } else {
+        oWins++
+    }
+    winCounter.innerText = "X Wins: " + xWins + "\nO Wins: " + oWins
+    gameInProgress = false
+}
 
 function resetGrid() {
     cell0.innerText = ''
@@ -100,6 +121,7 @@ function resetGrid() {
     gameInProgress = true
     moveCounter = 0
     console.log('**RESTART GAME**')
+    updateMessage.innerText = "New game started! X goes first."
 }
 
 
